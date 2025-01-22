971d1e6bed5257400d5488bdd4a87cc030710a93
jumarubea <rubeajuma8@gmail.com>
2025-01-22 23:14:01 +0300

    betty solved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-    write(STDOUT_FILENO, "and that piece of art is useful", 30);
-    write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 29);
+    write(STDOUT_FILENO,
+          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+          59);
     return (1);
 }
