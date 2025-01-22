4521e126da944d5f839caa362a6cb0e184e1ddad
jumarubea <rubeajuma8@gmail.com>
2025-01-22 23:09:35 +0300

    betty solved

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
-#include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 1 (Failure)
  */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\
-\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+    write(STDOUT_FILENO, "and that piece of art is useful", 30);
+    write(STDOUT_FILENO, "\" - Dora Korpar, 2015-10-19\n", 29);
+    return (1);
 }
