7b0cf294ce186d008407b24ee2fe3aa1959e3e5e
jumarubea <rubeajuma8@gmail.com>
2025-01-24 11:18:28 +0300

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hex[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 
+		0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
+
+	int size = sizeof(hex) / sizeof(hex[0]);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (hex[i] < 10)
+			putchar(hex[i] + '0');
+		else
+			putchar(hex[i] - 10 + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
