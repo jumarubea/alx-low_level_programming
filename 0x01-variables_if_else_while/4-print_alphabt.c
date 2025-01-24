8d9a636ef2d8b9dad55d7f2217b61971602df54f
jumarubea <rubeajuma8@gmail.com>
2025-01-24 10:15:56 +0300

    except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = {'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+	int size = sizeof(a);
+
+	for (i = 0; i < size; i++)
+	{
+		if (a[i] == 'e' || a[i] == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(a[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
