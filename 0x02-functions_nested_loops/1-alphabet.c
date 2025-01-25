678b1a52ef6480836fc9b757c6d8536f56cc3ef4
jumarubea <rubeajuma8@gmail.com>
2025-01-26 01:11:59 +0300

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * print_alphabet - print whole letter
- * Return: void
- */
-void print_alphabet(void)
-{
-        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f',
-                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                'x', 'y',  'z'};
-        int i;
-	int size = sizeof(arr) / sizeof(arr[0]);
-
-        for (i = 0; i < size; i++)
-        {
-                _putchar(arr[i]);
-        }
-        _putchar('\n');
-}
-
-/**
  * main - check the code
  *
  * Return: Always 0.
