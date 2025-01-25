ed6e91c32f47577c802eecc7fb9f11ee1b39fcd9
jumarubea <rubeajuma8@gmail.com>
2025-01-26 01:17:54 +0300

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,3 +11,23 @@ int main(void)
     print_alphabet();
     return (0);
 }
+
+/**
+ * print_alphabet - display alphabets
+ * return: void
+ */
+void print_alphabet(void)
+{
+        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f',
+                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                'x', 'y',  'z'};
+        int i;
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+        for (i = 0; i < size; i++)
+        {
+                _putchar(arr[i]);
+        }
+        _putchar('\n');
+}
