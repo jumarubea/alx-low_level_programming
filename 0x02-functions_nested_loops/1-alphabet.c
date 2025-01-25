94be6b2b5ec2ff74ddccf50cc5c411c49f9516ae
jumarubea <rubeajuma8@gmail.com>
2025-01-26 01:25:12 +0300

    printing alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * print_alphabet - display alphabets
- * return: void
- */
-void print_alphabet(void)
-{
-        char arr[] = {'a', 'b', 'c', 'd', 'e', 'f',
-                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                'x', 'y',  'z'};
-        int i;
-        int size = sizeof(arr) / sizeof(arr[0]);
-
-        for (i = 0; i < size; i++)
-        {
-                _putchar(arr[i]);
-        }
-        _putchar('\n');
-}
-
-/**
  * main - check the code
  *
  * Return: Always 0.
@@ -28,6 +8,23 @@ void print_alphabet(void)
 
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	void print_alphabet(void)
+	{
+		char arr[] = {'a', 'b', 'c', 'd', 'e', 'f',
+                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                'x', 'y',  'z'};
+		
+		int i;
+		int size = sizeof(arr) / sizeof(arr[0]);
+		
+		for (i = 0; i < size; i++)
+		{
+			_putchar(arr[i]);
+		}
+		
+		_putchar('\n');
+	}
+	print_alphabet();
+	return (0);
 }
