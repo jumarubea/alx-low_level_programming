20bc896e7d7befebec6536b4569bda5a35b1e1c9
jumarubea <rubeajuma8@gmail.com>
2025-01-26 17:09:26 +0300

    Im curious why not params

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - compute absolute value
+ *
+ * Description: return always return 0 or +ve
+ * @n: int value
+ *
+ * Return: int value as value of abs of n
+ */
+
+int _abs(int n)
+{
+	return ((n >= 0) ? n : -1 * n);
+}
