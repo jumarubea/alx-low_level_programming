1dfca91780ef8a20b1649985199fa855a0d3664a
jumarubea <rubeajuma8@gmail.com>
2025-01-26 16:21:48 +0300

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _isalpha - function to check (a-zA-Z)
+ *
+ * Description: it is function
+ * @c : params
+ * 
+ * Return: 1 if satisfy and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z');
+}
