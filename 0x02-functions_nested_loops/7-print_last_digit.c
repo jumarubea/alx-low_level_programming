eb62cf29d2c37259f973323e01af0aa5e5346a5a
jumarubea <rubeajuma8@gmail.com>
2025-01-26 17:14:50 +0300

    nice floooow

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/**
+ * print_last_digit - function to return the last digit
+ *
+ * Description: a just a modulus of 10
+ * @n: takes inputs as number
+ *
+ * Return: int value that is last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
