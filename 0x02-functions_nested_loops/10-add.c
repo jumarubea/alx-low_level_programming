4de14b95cb21ff2f9ba9b04228e1f2ae52548aee
jumarubea <rubeajuma8@gmail.com>
2025-01-26 18:56:20 +0300

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - accept and add function
+ *
+ * Descrition: just adding
+ * @num1: first args
+ * @num2: sec args
+ *
+ * Return: int value
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
