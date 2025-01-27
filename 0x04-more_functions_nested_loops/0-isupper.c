9875996080c4516f63620b22509263366924c2d5
jumarubea <rubeajuma8@gmail.com>
2025-01-27 09:46:06 +0300

    new yearrr

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - just a function
+ *
+ * Description: 'a function to check it is upper or not'
+ * @c: int input
+ *
+ * Return: whether 1 or 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
