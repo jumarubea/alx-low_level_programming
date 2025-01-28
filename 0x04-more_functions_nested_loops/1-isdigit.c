6223cb0340d0ebcd8d1149c0d2fa11b602ec7858
jumarubea <rubeajuma8@gmail.com>
2025-01-28 19:42:05 +0300

    a digit message

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - to print a digit
+ *
+ * Description: a fuctions
+ * @c: a character
+ *
+ * Return: a 1 if true and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 127);
+}
