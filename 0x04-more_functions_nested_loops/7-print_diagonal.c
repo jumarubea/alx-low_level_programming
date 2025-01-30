603835a8fdccf36363fb79a47f9b7e9d3d9d9957
jumarubea <rubeajuma8@gmail.com>
2025-01-30 17:29:05 +0300

    hard to think

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print a diagonal
+ * Description: a diagonal
+ * @n: number
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+
+	else
+		_putchar('\n');
+}
