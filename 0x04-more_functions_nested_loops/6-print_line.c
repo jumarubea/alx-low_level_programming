ffba9c9d8452db8834245152780968c34d723911
jumarubea <rubeajuma8@gmail.com>
2025-01-30 16:59:26 +0300

    printline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * Description: a just
+ * @n: length of the line
+ * Return: a line
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	else
+		_putchar('\n');
+}
