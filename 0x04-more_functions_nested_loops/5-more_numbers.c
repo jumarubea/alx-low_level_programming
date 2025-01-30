c1ac48d755d35266c2c9653e928833d3fe417183
jumarubea <rubeajuma8@gmail.com>
2025-01-30 16:37:35 +0300

    number 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14, 10 times
+ * Return: numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= 10; i++)
+	{
+		for (j = '0'; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
